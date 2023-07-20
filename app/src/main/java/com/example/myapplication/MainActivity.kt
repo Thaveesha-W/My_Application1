494ff660f13a0f6514fb39f8c2daacbc9b2e92e3
Sadaranga Hennedige <sadaranga@ceylonsolutions.com>
2023-07-20 13:16:17 +0530

    Implementing feature

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        print("HELLO WORLD")
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
